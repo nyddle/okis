19d88672660f2014c48b66e74a1f50ff75ffe244
Alexander Davydov <nyddle@gmail.com>
2014-11-25 01:18:42 +0300

    add core urls

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.shortcuts import render_to_response
+
+from django.views.generic.base import TemplateView
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'okis.views.home', name='home'),
+    url(r'^', TemplateView.as_view(template_name='home.html'), name='home'),
+)

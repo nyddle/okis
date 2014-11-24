c6baf633da51780f8a71b6ae3928684a84e797e4
Alexander Davydov <nyddle@gmail.com>
2014-11-24 22:40:40 +0300

    initial commit

---
+++ okis/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for okis project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "okis.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()

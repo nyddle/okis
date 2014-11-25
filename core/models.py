b176007c70149efe14e59e051f30f294aa82aa82
Alexander Davydov <nyddle@gmail.com>
2014-11-25 23:37:22 +0300

    add templates class

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+OKIS_THEMES = {
+    'auto' : u'Автомобили',
+    'business' : u'Бизнес',
+    'computers' : u'Компьютеры',
+    'sport' : u'Спорт'
+}
+
+class OkisTemplate(models.Model):
+    name = models.CharField(max_length=200)
+    html = models.CharField(max_length=200)
+    css  = models.CharField(max_length=200)
+    theme = models.CharField(max_length=200)
+
+

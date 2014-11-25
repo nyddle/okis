6bff3a3cf1b44b66cf05b8b062cf790039bb7a65
Alexander Davydov <nyddle@gmail.com>
2014-11-25 23:56:58 +0300

    add ThemesView

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
+
+from django.http import Http404
+from django.http import HttpResponseNotFound
+
+from django.views.generic import View
+from django.views.generic import ListView
+from django.views.generic.edit import FormView, ProcessFormView, CreateView
+
+from .models import OkisTemplate, OKIS_THEMES
+
+
+class ThemesView(View):
+
+    def get(self, request):
+        return render(request, 'reg.html', {'themes': OKIS_THEMES })
+
+

c059bd873aff423c49bad3c525bcc23f7ce69790
Alexander Davydov <nyddle@gmail.com>
2014-12-02 00:40:21 +0300

    add UserAdminView

--- useradmin/views.py
+++ useradmin/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import View
 
-# Create your views here.
+
+class UserAdminView(View):
+
+    def get(self, request):
+        if self.request.user.is_authenticated():
+            return render(request, 'useradmin/admin.html')
+        else:
+            return redirect('account_login')
 
